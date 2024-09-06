4bf5a076303a2b7fd55f0fb1fb3e05bc4c875660
liupeng <1269861316@qq.com>
2024-09-06 22:03:40 +0800

    fixed

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: sky
  * @Date: 2024-08-26 17:55:05
- * @LastEditTime: 2024-09-06 21:31:34
+ * @LastEditTime: 2024-09-06 22:03:06
  * @LastEditors: sky
  */
 // 注意要安装@actions/github依赖
@@ -12,8 +12,6 @@ import fs from 'fs';
 
 // 在容器中可以通过env环境变量来获取参数
 const octokit = getOctokit(process.env.GITHUB_TOKEN);
-
-
 
 const getLatestDraftRelease = async () => {
   // 列出所有release
@@ -29,7 +27,6 @@ const getLatestDraftRelease = async () => {
   return draftReleases[0] || null;
 };
 
-
 const updateRelease = async () => {
   // 获取updater tag的release
   const { data: release } = await octokit.rest.repos.getReleaseByTag({
@@ -40,23 +37,24 @@ const updateRelease = async () => {
 
   const Latest = await getLatestDraftRelease();
 
-  // 需要生成的静态 json 文件数据，根据自己的需要进行调整
-  const updateData = {
-    version: Latest.tag_name.replace(/^v/, ''),
-    // 使用 UPDATE_LOG.md，如果不需要版本更新日志，则将此字段置空
-    notes: "",
-    pub_date: new Date().toISOString(),
-    platforms: {
-      win64: { signature: '', url: '' }, // compatible with older formats
-      linux: { signature: '', url: '' }, // compatible with older formats
-      darwin: { signature: '', url: '' }, // compatible with older formats
-      'darwin-aarch64': { signature: '', url: '' },
-      'darwin-x86_64': { signature: '', url: '' },
-      'linux-x86_64': { signature: '', url: '' },
-      'windows-x86_64': { signature: '', url: '' },
-      // 'windows-i686': { signature: '', url: '' }, // no supported
-    },
-  };
+  if (!Latest) {
+    console.log('No draft release found.');
+    return;
+  }
+
+  // 从Assets中读取latest.json文件
+  const latestJsonAsset = Latest.assets.find(asset => asset.name === "latest.json");
+
+  if (!latestJsonAsset) {
+    console.log('latest.json not found in assets.');
+    return;
+  }
+
+  const latestJsonResponse = await octokit.request(`GET ${latestJsonAsset.browser_download_url}`);
+  const updateData = JSON.parse(latestJsonResponse.data);
+
+  // 更新version
+  updateData.version = Latest.tag_name.replace(/^v/, '');
 
   // 删除旧的的文件
   const deletePromises = release.assets
@@ -69,44 +67,7 @@ const updateRelease = async () => {
       });
     });
 
-    await Promise.all(deletePromises);
-
-  const setAsset = async (asset, reg, platforms) => {
-    let sig = '';
-    if (/.sig$/.test(asset.name)) {
-      sig = await getSignature(asset.browser_download_url);
-    }
-    platforms.forEach((platform) => {
-      if (reg.test(asset.name)) {
-        // 设置平台签名，检测应用更新需要验证签名
-        if (sig) {
-          updateData.platforms[platform].signature = sig;
-          return;
-        }
-        // 设置下载链接
-        updateData.platforms[platform].url = asset.browser_download_url;
-      }
-    });
-  };
-
-  const promises = Latest.assets.map(async (asset) => {
-    // windows
-    await setAsset(asset, /.msi.zip/, ['win64', 'windows-x86_64']);
-
-    // darwin
-    await setAsset(asset, /.app.tar.gz/, [
-      'darwin',
-      'darwin-x86_64',
-      'darwin-aarch64',
-    ]);
-
-    // linux
-    await setAsset(asset, /.AppImage.tar.gz/, ['linux', 'linux-x86_64']);
-  });
-
-  await Promise.allSettled(promises);
-
-
+  await Promise.all(deletePromises);
 
   // 将数据写入文件
   fs.writeFileSync(
@@ -126,17 +87,3 @@ const updateRelease = async () => {
   });
 };
 updateRelease().catch(console.error);
-
-
-// 获取签名内容
-async function getSignature(url) {
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/octet-stream' },
-    });
-    return response.text();
-  } catch (_) {
-    return '';
-  }
-}
